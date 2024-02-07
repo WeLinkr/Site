ef8c0ecd6b37cddaf00b3bd3ddd1a30ef668a206
Rachael Bonnie <145918057+cmd-rgb@users.noreply.github.com>
2024-02-07 09:24:42 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
+
 const ComingSoonPage: React.FC = () => {
   return (
     <body>
       <img src="site.png" alt="WeLinkr: Connect, share, and discover."></img>
+      <a href="https://twitter.com/WeLinkrSocial" target="_blank" rel="noopener noreferrer">Follow us on Twitter</a>
     </body>
   );
 };
